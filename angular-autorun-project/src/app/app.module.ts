a4c1cf3d9ef4968964fe98234dc8703a0e254b82
Lukasz Dunat <lukaszxdunat@gmail.com>
2023-10-15 19:45:22 +0200

    Created header component and added routing

--- angular-autorun-project/src/app/app.module.ts
+++ angular-autorun-project/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { ItemListComponent } from './items/item-list/item-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemListComponent
+    ItemListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ItemListComponent},
+      { path: '**', redirectTo: 'products', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
